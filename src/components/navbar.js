8533a66e33d0e8e24f7b7646e2e849ddce93b08b
John Ho <jho71@myseneca.ca>
2021-02-22 14:46:49 -0500

    hashRouter

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,9 +6,9 @@ import '../App.css'
 const Navibar = () => (
     <Navbar bg="light" variant="light">
         <Nav>
-            <Navbar.Brand href="/#/WeStandWithNurses"><img src={Logo} width="30em" alt= "logo" id="_logo"></img></Navbar.Brand>
+            <Navbar.Brand href="#/"><img src={Logo} width="30em" alt= "logo" id="_logo"></img></Navbar.Brand>
             <Nav.Item>
-                <Nav.Link href="/#/WeStandWithNurses/about" id="media_title">About Us</Nav.Link>
+                <Nav.Link href="#/about" id="media_title">About Us</Nav.Link>
             </Nav.Item>
             <Dropdown>
             <Dropdown.Toggle  id="dropdown-basic">
@@ -16,16 +16,16 @@ const Navibar = () => (
             </Dropdown.Toggle>
 
             <Dropdown.Menu>
-                <Dropdown.Item href="/#/WeStandWithNurses/statistics">Statistics of How Covid-19 Impacts Nurses</Dropdown.Item>
-                <Dropdown.Item href="/#/#WeStandWithNurses/effects">Effects of Covid-19 on Nurses and Why The Public Should Care</Dropdown.Item>
-                <Dropdown.Item href="/#/#WeStandWithNurses/howtohelp">How You Can Help Nurses</Dropdown.Item>
+                <Dropdown.Item href="#/statistics">Statistics of How Covid-19 Impacts Nurses</Dropdown.Item>
+                <Dropdown.Item href="#/effects">Effects of Covid-19 on Nurses and Why The Public Should Care</Dropdown.Item>
+                <Dropdown.Item href="#/howtohelp">How You Can Help Nurses</Dropdown.Item>
             </Dropdown.Menu>
             </Dropdown>
             <Nav.Item>
-                <Nav.Link href="/#/WeStandWithNurses/quiz" id="media_title">Take Quiz</Nav.Link>
+                <Nav.Link href="#/quiz" id="media_title">Take Quiz</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link href="#/WeStandWithNurses/contact" id="media_title">Contact Us</Nav.Link>
+                <Nav.Link href="#/contact" id="media_title">Contact Us</Nav.Link>
             </Nav.Item>
         </Nav>
     </Navbar>
