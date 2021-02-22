dd50bebbb48ddf54de57602dd202bc210313acec
John Ho <jho71@myseneca.ca>
2021-02-22 14:03:39 -0500

    Updates

---
+++ precache-manifest.752b55e1e3ed689db385b9fa81729d44.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdf594d347fe3247b687a152b72262fb",
+    "url": "/WeStandWithNurses/index.html"
+  },
+  {
+    "revision": "190b28aa0ce12e7703ac",
+    "url": "/WeStandWithNurses/static/css/2.b0704b7a.chunk.css"
+  },
+  {
+    "revision": "19e407dbb1a12726fa69",
+    "url": "/WeStandWithNurses/static/css/main.2ef436ad.chunk.css"
+  },
+  {
+    "revision": "190b28aa0ce12e7703ac",
+    "url": "/WeStandWithNurses/static/js/2.ce76319b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/WeStandWithNurses/static/js/2.ce76319b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19e407dbb1a12726fa69",
+    "url": "/WeStandWithNurses/static/js/main.8d28952d.chunk.js"
+  },
+  {
+    "revision": "03f60aefa6eee1d6ad6a",
+    "url": "/WeStandWithNurses/static/js/runtime-main.ae66c914.js"
+  },
+  {
+    "revision": "93d69ea53cd029ae5b38ccfb188a8429",
+    "url": "/WeStandWithNurses/static/media/Covid19Background-.93d69ea5.jpg"
+  },
+  {
+    "revision": "2d4be761a81fa074c9fa4105ee86b2d1",
+    "url": "/WeStandWithNurses/static/media/covid19.2d4be761.png"
+  },
+  {
+    "revision": "534d309c6a1ae74e3995d242d2668714",
+    "url": "/WeStandWithNurses/static/media/sideBySide.534d309c.jpg"
+  }
+]);
